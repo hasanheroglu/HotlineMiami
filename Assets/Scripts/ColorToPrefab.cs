aa794c0f5358cd747c2e32f7146323ad857e5a38
hasanheroglu <hasanheroglu@gmail.com>
2018-10-24 15:56:27 +0300

    Still working on main level
    -Added Level Generator
    -Added ColorToPrefab
    -Added SpriteLibrary (Not sure about using it)

---
+++ Assets/Scripts/ColorToPrefab.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Level{
+	[System.Serializable]
+	public class ColorToPrefab{
+		public Color color;
+		public GameObject prefab;
+		
+	}
+}
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -8,15 +8,35 @@ namespace Level{
 
 		private SpriteLibrary spriteLibrary;
 
+		public Texture2D levelMap;
+		public ColorToPrefab[] prefabs;
+
 		// Use this for initialization
 		void Start () {
 			spriteLibrary = GameObject.FindWithTag("SpriteLibrary").GetComponent<SpriteLibrary>();
-			Debug.Log(spriteLibrary.corner.border.y);
+			GenerateLevel();
 		}
 		
 		// Update is called once per frame
 		void Update () {
 			
+		}
+
+		private void GenerateLevel(){
+			for(int i=0; i < levelMap.width; i++){
+				for(int j=0; j < levelMap.height; j++){
+					GenerateObject(levelMap.GetPixel(i,j), i, j);
+				}
+			}
+		}
+
+		private void GenerateObject(Color c, int x, int y){
+			for(int i = 0; i < prefabs.Length; i++){
+				if(prefabs[i].color == c){
+					Debug.Log("Girdi");
+					Instantiate(prefabs[i].prefab, new Vector3(x , y , 0), Quaternion.identity);
+				}
+			}
 		}
 	}
 
