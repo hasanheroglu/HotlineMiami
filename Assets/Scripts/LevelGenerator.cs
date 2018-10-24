6f46b173db98e0569404b71539fabfb51dc46f1a
hasanheroglu <hasanheroglu@gmail.com>
2018-10-24 14:03:45 +0300

    Commit#2
    -Added some sprites
    -Trying to build Level01

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Image;
+
+namespace Level{
+	public class LevelGenerator : MonoBehaviour {
+
+		private SpriteLibrary spriteLibrary;
+
+		// Use this for initialization
+		void Start () {
+			spriteLibrary = GameObject.FindWithTag("SpriteLibrary").GetComponent<SpriteLibrary>();
+			Debug.Log(spriteLibrary.corner.border.y);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+
+}
---
+++ Assets/Scripts/SpriteLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Image
+{
+	
+	public class SpriteLibrary : MonoBehaviour {
+
+		public Sprite floor;
+		public Sprite wall;
+		public Sprite corner;
+		public Sprite player;
+		public Sprite crosshair;
+	}
+}
