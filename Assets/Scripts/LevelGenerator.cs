45bb04ab11f7f74e12d74e86110f2a43687f4c7d
hasanheroglu <hasanheroglu@gmail.com>
2018-10-25 09:13:58 +0300

    Fixes and new features
    -Sprites renewed. (Using 4x4 bit sprites right now. Probably upgrade that to 8x8 or 16x16)
    -Level Generator works now! You can create your level on GIMP or some program that you can create pixels.
    -PlayerMovement script added. Now, player can move, rotate with mouse(buggy but will be fixed).

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -6,38 +6,43 @@ using Image;
 namespace Level{
 	public class LevelGenerator : MonoBehaviour {
 
-		private SpriteLibrary spriteLibrary;
+		//private SpriteLibrary spriteLibrary;
 
 		public Texture2D levelMap;
 		public ColorToPrefab[] prefabs;
 
 		// Use this for initialization
 		void Start () {
-			spriteLibrary = GameObject.FindWithTag("SpriteLibrary").GetComponent<SpriteLibrary>();
+			//spriteLibrary = GameObject.FindWithTag("SpriteLibrary").GetComponent<SpriteLibrary>();
 			GenerateLevel();
 		}
-		
-		// Update is called once per frame
-		void Update () {
-			
-		}
-
 		private void GenerateLevel(){
+			List <float> positions = new List<float>{
+				0,0
+			};
 			for(int i=0; i < levelMap.width; i++){
 				for(int j=0; j < levelMap.height; j++){
-					GenerateObject(levelMap.GetPixel(i,j), i, j);
+					GenerateObject(i, j, positions);
 				}
 			}
 		}
 
-		private void GenerateObject(Color c, int x, int y){
+		private void GenerateObject(int x, int y, List<float> positions){
+			
+			Color c = levelMap.GetPixel(x,y);
+			float location_multiplier_x = 0;
+			float location_multiplier_y = 0;
 			for(int i = 0; i < prefabs.Length; i++){
 				if(prefabs[i].color == c){
-					Debug.Log("Girdi");
-					Instantiate(prefabs[i].prefab, new Vector3(x , y , 0), Quaternion.identity);
+					location_multiplier_x = prefabs[i].prefab.GetComponent<SpriteRenderer>().sprite.rect.width/100;
+					location_multiplier_y = prefabs[i].prefab.GetComponent<SpriteRenderer>().sprite.rect.height/100;
+					Instantiate(prefabs[i].prefab, 
+					new Vector3(x * location_multiplier_x ,y * location_multiplier_y , 0), 
+					prefabs[i].prefab.transform.rotation);
 				}
 			}
+			positions[0] += x * location_multiplier_x / 2;
+			positions[1] += y * location_multiplier_y / 2;
 		}
 	}
-
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Player{
+	public class PlayerMovement : MonoBehaviour {
+
+		private Rigidbody2D rigidbody;
+
+
+		[Range(1f, 10f)]
+		public float movementSpeed = 6f;
+
+		[Range(2f, 5f)]
+		public float movementSpeedMultiplier = 3f;
+
+		public float rotationSpeed = 20f;
+
+        // Use this for initialization
+        private void Start () {
+			rigidbody = GetComponent<Rigidbody2D>();
+		}
+		
+		// Update is called once per frame
+		private void Update () {
+			RotateWithMouse();
+		}
+
+		private void FixedUpdate(){
+			Move();
+		}
+
+		private void Move(){
+			float move_x = Input.GetAxis("Horizontal");
+			float move_y = Input.GetAxis("Vertical");
+			
+			if(Input.GetKey(KeyCode.LeftShift))
+				rigidbody.velocity = CalculateVelocity(move_x, move_y, movementSpeed * movementSpeedMultiplier);
+			else
+				rigidbody.velocity = CalculateVelocity(move_x, move_y, movementSpeed);
+		} 
+
+		private Vector3 CalculateVelocity(float _move_x, float _move_y, float movementSpeed){
+			return new Vector3(_move_x * movementSpeed * Time.deltaTime,
+							   _move_y * movementSpeed * Time.deltaTime,
+							   0);
+		}
+
+		
+
+		private void RotateWithMouse(){
+			Vector3 mousePosition = Input.mousePosition;
+			mousePosition.z = 1;
+			mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+			
+			Vector2 direction = new Vector2(mousePosition.x - transform.localPosition.x,
+											mousePosition.y - transform.localPosition.y);
+
+			Debug.DrawLine(transform.position, mousePosition, Color.red, 10f);
+
+			float angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
+			Quaternion rotation = Quaternion.AngleAxis(angle - 74f, Vector3.forward);
+			Debug.Log(rotation);
+			transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed *  Time.deltaTime);
+		}
+	}
+}
