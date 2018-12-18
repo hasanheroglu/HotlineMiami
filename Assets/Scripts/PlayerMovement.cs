c689195e9e6ab3ed3456e650cc6747b4bb125954
hasanheroglu <hasanheroglu@gmail.com>
2018-12-18 13:14:37 +0300

    Player Rotation and Sorting Layers
    -Fixed Player Rotation.
    -Added sorting layers for sprites.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 namespace Player{
 	public class PlayerMovement : MonoBehaviour {
 
-		private Rigidbody2D rigidbody;
-
-
+		
 		[Range(1f, 10f)]
-		public float movementSpeed = 6f;
+		private float movementSpeed = 6f;
 
 		[Range(2f, 5f)]
-		public float movementSpeedMultiplier = 3f;
+		private float movementSpeedMultiplier = 3f;
+		
+		private float rotationSpeed = 10f;
 
-		public float rotationSpeed = 20f;
+		public Rigidbody2D rigidbody;
+
 
         // Use this for initialization
         private void Start () {
@@ -30,37 +31,31 @@ namespace Player{
 		}
 
 		private void Move(){
-			float move_x = Input.GetAxis("Horizontal");
-			float move_y = Input.GetAxis("Vertical");
+			var moveX = Input.GetAxis("Horizontal");
+			var moveY = Input.GetAxis("Vertical");
 			
-			if(Input.GetKey(KeyCode.LeftShift))
-				rigidbody.velocity = CalculateVelocity(move_x, move_y, movementSpeed * movementSpeedMultiplier);
-			else
-				rigidbody.velocity = CalculateVelocity(move_x, move_y, movementSpeed);
+			rigidbody.velocity = Input.GetKey(KeyCode.LeftShift) ? 
+								 CalculateVelocity(moveX, moveY, movementSpeed * movementSpeedMultiplier) : 
+								 CalculateVelocity(moveX, moveY, movementSpeed);
 		} 
 
-		private Vector3 CalculateVelocity(float _move_x, float _move_y, float movementSpeed){
-			return new Vector3(_move_x * movementSpeed * Time.deltaTime,
-							   _move_y * movementSpeed * Time.deltaTime,
+		private Vector3 CalculateVelocity(float moveX, float moveY, float movSpeed){
+			return new Vector3(moveX * movSpeed * Time.deltaTime,
+							   moveY * movSpeed * Time.deltaTime,
 							   0);
 		}
 
-		
-
 		private void RotateWithMouse(){
-			Vector3 mousePosition = Input.mousePosition;
+			var mousePosition = Input.mousePosition;
 			mousePosition.z = 1;
 			mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+			var direction = mousePosition - transform.position;
+			direction = direction.normalized;
 			
-			Vector2 direction = new Vector2(mousePosition.x - transform.localPosition.x,
-											mousePosition.y - transform.localPosition.y);
-
-			Debug.DrawLine(transform.position, mousePosition, Color.red, 10f);
-
-			float angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
-			Quaternion rotation = Quaternion.AngleAxis(angle - 74f, Vector3.forward);
-			Debug.Log(rotation);
-			transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed *  Time.deltaTime);
+			var angle = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg;
+			var rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			transform.rotation = Quaternion.Euler(0,0, angle);
 		}
 	}
 }
